5a9886efb859b8ba21d4207baf3af8cff8c1937b
imhenno <hendrikredenius1@gmail.com>
2023-02-01 17:05:36 +0100

    warum hatte ich andere versionen

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,36 @@
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import express, {Request, Response} from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
+import express, { Request, Response } from "express";
 
-const app = express()                                        
+const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
-
-app.get('/throw', (req: Request, res: Response) => {
-  res.send("" + Math.floor(Math.random() * 61))
-})
-
-io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`);
-
-  socket.on('beispiel-event', (zahl: number) => {
-    console.log(`So macht man Events. Zahl ${zahl} wurde gesendet`);
-  })
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
 });
 
-app.listen(8081)
-console.log("Server läuft auf 8081")
+app.get("/throw", (req: Request, res: Response) => {
+    res.send("" + Math.floor(Math.random() * 61));
+});
+
+io.on("connection", (socket) => {
+    console.log(`${socket.id} connected`);
+
+    socket.on("ping", () => {
+        socket.emit("pong", "Das Event wurde getriggert :)")
+    })
+
+    socket.on("join-room", (room: string) => {
+        socket.join(room);
+        console.log(`${socket.id} joined ${room}`);
+    });
+
+    socket.on("dart-throw", (room: string, player: number, points: number) => {
+        io.to(room).emit("dart-throw", player, points);
+    });
+});
+
+httpServer.listen(8081, () => {
+    console.log("Server läuft auf 8081");
+});
