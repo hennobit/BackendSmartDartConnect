0c4f7b871f1322784d263fa80f89a0815724763e
hennobit <hendrikredenius1@gmail.com>
2023-02-20 22:32:28 +0100

    exception handling soweit eingebaut

--- src/interfaces/game.ts
+++ src/interfaces/game.ts
@@ -1,6 +1,10 @@
-import { Socket } from 'socket.io';
-import { Player } from './player';
-import { globalGameMap, globalPlayerMap, handleGameRooms } from '../utils/sessionmanager';
+import { Socket } from "socket.io";
+import { Player } from "./player";
+import {
+    globalGameMap,
+    globalPlayerMap,
+    handleGameRooms,
+} from "../utils/sessionmanager";
 
 interface GameStart {
     roomId: string;
@@ -25,7 +29,7 @@ export function startGame(socket: Socket, json: string): void {
     let gameStart: GameStart | undefined = undefined;
     gameStart = JSON.parse(json);
     if (!gameStart) {
-        console.log('Start-Game JSON kaputt');
+        console.log("Start-Game JSON kaputt");
         return;
     }
     console.log(gameStart);
@@ -36,7 +40,7 @@ export function startGame(socket: Socket, json: string): void {
         players: players,
         currentPlayer: players[0],
         nextPlayer: players[1] ? players[1] : players[0],
-        dartsLeft: 3
+        dartsLeft: 3,
     };
     handleGameRooms(socket, game);
 }
@@ -49,17 +53,24 @@ export function dartThrown(socket: Socket, json: string): void {
         console.log(err);
     }
     if (!dartThrow) {
-        console.log('Dart-Throw JSON kaputt');
+        console.log("Dart-Throw JSON kaputt");
         return;
     }
-    console.log(dartThrow.roomId)
     let game: Game = globalGameMap.get(dartThrow.roomId);
-    let playerInRoomArray: Player[] = game.players;
-    let player: Player | undefined = playerInRoomArray.find((p: Player) => p.socket === dartThrow?.player.socket);
-
-    if (player === undefined) {
+    if (!game) {
         console.log(
-            `Error in Raum ${dartThrow.roomId}! Spieler mit der SocketID ${dartThrow.player.socket} nicht gefunden.`
+            `Error Game.ts => dartThrown(). Es wurde kein aktives Spiel mit der ID ${dartThrow.roomId} gefunden!`
+        );
+        return;
+    }
+    let playerInRoomArray: Player[] = game.players;
+    let player: Player | undefined = playerInRoomArray.find(
+        (p: Player) => p.socket === dartThrow?.player.socket
+    );
+
+    if (!player) {
+        console.log(
+            `Error Game.ts => dartThrown(). Spieler mit der SocketID ${dartThrow.player.socket} in Raum ${dartThrow.roomId} nicht gefunden!`
         );
         return;
     }
@@ -77,40 +88,39 @@ export function dartThrown(socket: Socket, json: string): void {
     }
 
     if (game.dartsLeft === 0) {
-        game = nextTurn(game);
+        game = rotatePlayers(game);
     }
     console.log(
-        `${dartThrow.points * dartThrow.multiplikator} geworfen von ${player.name} in Raum ${game.roomId}. ${player.pointsLeft} Punkte verbleiben...`
+        `${dartThrow.points * dartThrow.multiplikator} geworfen von ${
+            player.name
+        } in Raum ${game.roomId}. ${player.pointsLeft} Punkte verbleiben...`
     );
-    socket.to(game.roomId).emit('dart-throw', JSON.stringify(game));
+    socket.to(game.roomId).emit("dart-throw", JSON.stringify(game));
 }
 
 function isLegalThrow(player: Player, dartThrow: DartThrow): boolean {
-    if (player.pointsLeft - dartThrow.points < 0) {
-        return false;
-    }
-    return true;
+    // wenn players points unter 0 gehen dann illegaler throw
+    return !(player.pointsLeft - dartThrow.points * dartThrow.multiplikator < 0)
 }
 
-function handleIllegalThrow(socket: Socket, player: Player, game: Game): void {
-    game.dartsLeft = 0;
-    console.log(`${game.currentPlayer.name} hat in ${game.roomId} über 0 geworfen`);
-    console.log(`Er bleibt bei ${player.pointsLeft} Punkten. Nächster`);
-    socket.to(game.roomId).emit('dart-throw', JSON.stringify(game));
-}
-
-function nextTurn(game: Game): Game {
-    let currentPlayerIndex = game.players.indexOf(game.currentPlayer);
-    currentPlayerIndex = (currentPlayerIndex + 1) % game.players.length;
-    game.currentPlayer = game.players[currentPlayerIndex];
-    game.nextPlayer = game.players[(currentPlayerIndex + 1) % game.players.length];
+function rotatePlayers(game: Game): Game {
+    const currentPlayerIndex = game.players.indexOf(game.currentPlayer);
+    game.currentPlayer = game.players[(currentPlayerIndex + 1) % game.players.length];
+    game.nextPlayer = game.players[(currentPlayerIndex + 2) % game.players.length];
     game.dartsLeft = 3;
     return game;
-}
+  }
+  
+  function handleIllegalThrow(socket: Socket, player: Player, game: Game): void {
+    rotatePlayers(game);
+    console.log(`${game.currentPlayer.name} hat in ${game.roomId} über 0 geworfen`);
+    console.log(`Er bleibt bei ${player.pointsLeft} Punkten. Nächster`);
+    socket.to(game.roomId).emit("dart-throw", JSON.stringify(game));
+  }
 
 function isGameOver(socket: Socket, player: Player, game: Game): boolean {
     if (player.pointsLeft === 0) {
-        socket.to(game.roomId).emit('game-over', game);
+        socket.to(game.roomId).emit("game-over", game);
         console.log(`${player.name} hat in Raum ${game.roomId} gewonnen! GG`);
         return true;
     }
